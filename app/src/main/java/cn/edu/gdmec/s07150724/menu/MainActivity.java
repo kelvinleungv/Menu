8d30ba53a69cadb15ca59e778f020621ce2f3cde
kelvinleungv <568750031@qq.com>
2016-10-20 11:38:45 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150724/menu/MainActivity.java
@@ -0,0 +1,55 @@
+package cn.edu.gdmec.s07150724.menu;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=(TextView)findViewById(R.id.textView);
+        registerForContextMenu(tv1);
+
+    }
+    //创建选项菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+        menu.add(0,1,0,R.string.m3);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    //选项菜单被选中
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
+        return super.onOptionsItemSelected(item);
+
+    }
+    //创建上下文菜单
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+        menu.add(0,1,0,R.string.m3);
+
+    }
+    //上下文菜单被选中
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
+        return super.onContextItemSelected(item);
+    }
+}
